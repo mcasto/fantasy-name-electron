a9e04bd7b51898b937d83c173ee56476d9912ad3
Mike Casto <castoware@gmail.com>
2023-08-20 06:45:03 -0500

    Generate groups updated & updated TODOs

--- src-electron/api/generate-groups.js
+++ src-electron/api/generate-groups.js
@@ -1,30 +1,18 @@
-import { camelCase } from "lodash";
-
-import militaryUnit from "./util/group-names/military-units";
-import mysticOrder from "./util/group-names/mystic-order";
-import thievesAndAssassins from "./util/group-names/thieves-and-assassins";
-
-const generators = {
-  militaryUnit,
-  mysticOrder,
-  thievesAndAssassins,
-};
+import generateGroup from "./util/generate-group";
 
 const maxAttempts = 1000;
 
 export default async (db, config) => {
   if (!config) return [];
-
   const { group, num } = config;
+
+  const rec = await db.findOne({ table: "groups", group });
 
   const results = [];
   let attempts = 0;
   while (results.length < num || attempts > maxAttempts) {
     attempts++;
-
-    const result = await generators[camelCase(group)](db);
-
-    results.push(result);
+    results.push(await generateGroup(db, rec));
   }
 
   return results;
